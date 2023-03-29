e79dace40fb5b6b0ca6c316cdf8cadf7cd77e9f8
Felipe <felipemalentaq@hotmail.com>
2023-03-29 19:39:39 -0300

    requisito 02 e 04 em andamento

--- app/backend/src/tests/change.me.test.ts
+++ app/backend/src/tests/change.me.test.ts
@@ -7,39 +7,57 @@ import { app } from '../app';
 import Example from '../database/models/ExampleModel';
 
 import { Response } from 'superagent';
+import teamsModel from '../database/models';
+import  teamsService from '../services/teams.service';
+import teamsMock from './mock/teams.mock';
 
 chai.use(chaiHttp);
 
 const { expect } = chai;
 
-describe('Seu teste', () => {
-  /**
-   * Exemplo do uso de stubs com tipos
-   */
+// describe('Seu teste', () => {
+//   /**
+//    * Exemplo do uso de stubs com tipos
+//    */
 
-  // let chaiHttpResponse: Response;
+//   // let chaiHttpResponse: Response;
 
-  // before(async () => {
-  //   sinon
-  //     .stub(Example, "findOne")
-  //     .resolves({
-  //       ...<Seu mock>
-  //     } as Example);
-  // });
+//   // before(async () => {
+//   //   sinon
+//   //     .stub(Example, "findOne")
+//   //     .resolves({
+//   //       ...<Seu mock>
+//   //     } as Example);
+//   // });
 
-  // after(()=>{
-  //   (Example.findOne as sinon.SinonStub).restore();
-  // })
+//   // after(()=>{
+//   //   (Example.findOne as sinon.SinonStub).restore();
+//   // })
 
-  // it('...', async () => {
-  //   chaiHttpResponse = await chai
-  //      .request(app)
-  //      ...
+//   // it('...', async () => {
+//   //   chaiHttpResponse = await chai
+//   //      .request(app)
+//   //      ...
 
-  //   expect(...)
-  // });
+//   //   expect(...)
+//   // });
 
-  it('Seu sub-teste', () => {
-    expect(false).to.be.eq(true);
-  });
-});
+//   it('Seu sub-teste', () => {
+//     expect(false).to.be.eq(true);
+//   });
+// });
+
+// describe('Verifica se retorna todos os teams', () => {
+//   it('Retorna todos os teams como getAll', async () => {
+//     sinon.stub(teamsModel, 'getAll').resolves(teamsMock);
+
+//     const resultGetAll = await teamsService.getAll();
+
+//     expect(resultGetAll).to.equal(teamsMock);
+//   });
+
+//   it('', () => {
+
+//   });
+// });
+
---
+++ app/backend/src/tests/teams.test.ts
@@ -0,0 +1,87 @@
+import * as sinon from 'sinon';
+import * as chai from 'chai';
+// @ts-ignore
+import chaiHttp = require('chai-http');
+
+import { app } from '../app';
+import Example from '../database/models/ExampleModel';
+
+import { Response } from 'superagent';
+import teamsModel from '../database/models/Teams';
+import  teamsService from '../services/teams.service';
+import { teams, teams1 } from './mock/teams.mock';
+
+chai.use(chaiHttp);
+
+const { expect } = chai;
+
+// describe('Seu teste', () => {
+//   /**
+//    * Exemplo do uso de stubs com tipos
+//    */
+
+//   // let chaiHttpResponse: Response;
+
+  // before(async () => {
+  //   sinon
+  //     .stub(Example, "findOne")
+  //     .resolves({
+  //       ...<Seu mock>
+  //     } as Example);
+  // });
+
+  // after(()=>{
+  //   (Example.findOne as sinon.SinonStub).restore();
+  // })
+
+//   // it('...', async () => {
+//   //   chaiHttpResponse = await chai
+//   //      .request(app)
+//   //      ...
+
+//   //   expect(...)
+//   // });
+
+//   it('Seu sub-teste', () => {
+//     expect(false).to.be.eq(true);
+//   });
+// });
+
+describe('Verifica se retorna todos os teams', () => {
+  // it('Retorna todos os teams como getAll', async () => {
+    
+  //   sinon.stub(teamsModel, 'findAll').resolves(teamsMock as teamsModel[]);
+
+  //   const resultGetAll = await teamsService.getAll();
+
+  //   expect(resultGetAll).to.be.deep.equal(teamsMock);
+
+  //     sinon.restore();
+  // });
+  it('Retorna todos os teams como getAll', async () => {
+      before(async () => {
+    sinon.stub(teamsModel, 'findAll').resolves(teams as teamsModel[]);
+  });
+    const response = await chai.request(app).get('/teams');
+    
+    expect(response.status).to.be.equals(200);
+    expect(response.body).to.be.deep.equal(teams);
+    
+    sinon.restore();
+    
+  });
+});
+
+describe('Verifica se retorna um time', () => {
+  it('Retorna um time especifico pelo id', async () => {
+    before(async () => {
+      sinon.stub(teamsModel, 'findOne').resolves(teams as teamsModel[]);
+    });
+      const response = await chai.request(app).get('/teams/12');
+      
+      expect(response.status).to.be.equals(200);
+      expect(response.body).to.be.deep.equal(teams1);
+      
+      sinon.restore();
+  });
+});
