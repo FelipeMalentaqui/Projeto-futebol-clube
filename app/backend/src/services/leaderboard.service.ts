ea342a4d4a93e95287298d5d873350dd174a7a1e
Felipe <felipemalentaq@hotmail.com>
2023-04-05 19:55:50 -0300

    refatorando requisito 23

--- app/backend/src/services/leaderboard.service.ts
+++ app/backend/src/services/leaderboard.service.ts
@@ -1,4 +1,3 @@
-/* eslint-disable max-lines-per-function */
 import {
   totalGoals, goalsOpponent, diferenceGoals,
   victory, draw, defeat, totalPoints, utilizationTeam,
@@ -9,16 +8,13 @@ import { IMatches } from '../interface/IMatches';
 // import { IMatchtesTeam } from '../interface/IMatchesTeam';
 
 const getAll = async () => {
+  // const matchsTeam = help();
   const teams = await Teams.findAll();
   const matches = await MatchesModel.findAll();
-
-  const matchsTeam = teams
-    .map((team) => matches
-      .filter((match) => team.id === match.dataValues.homeTeamId));
-
+  const matchsTeam = teams.map((team) => matches
+    .filter((match) => team.id === match.dataValues.homeTeamId));
   const array = matchsTeam.map((e, index) => {
-    const obj = {
-      name: teams[index].teamName,
+    const obj = { name: teams[index].teamName,
       totalPoints: totalPoints(e as unknown as IMatches[]),
       totalGames: e.length,
       totalVictories: victory(e as unknown as IMatches[]),
@@ -27,12 +23,9 @@ const getAll = async () => {
       goalsFavor: e.reduce(totalGoals, 0),
       goalsOwn: e.reduce(goalsOpponent, 0),
       goalsBalance: diferenceGoals(e.reduce(totalGoals, 0), e.reduce(goalsOpponent, 0)),
-      efficiency: utilizationTeam(e as unknown as IMatches[], e as unknown as IMatches[]),
-    };
-    // console.log(obj);
+      efficiency: utilizationTeam(e as unknown as IMatches[], e as unknown as IMatches[]) };
     return obj;
   });
-
   return array;
 };
 
